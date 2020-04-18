836d19bb866c9e4e9114a6271168f0d55f35f89b
saanayy <sanay.nitjsr@gmail.com>
2020-04-18 23:53:38 +0530

    one signal integration - done

--- app/src/main/java/com/suvidha/ApplicationClass.java
+++ app/src/main/java/com/suvidha/ApplicationClass.java
@@ -2,6 +2,7 @@ package com.suvidha;
 
 import android.app.Application;
 
+import com.onesignal.OSPermissionSubscriptionState;
 import com.onesignal.OneSignal;
 
 public class ApplicationClass extends Application {
@@ -13,5 +14,17 @@ public class ApplicationClass extends Application {
                 .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
                 .unsubscribeWhenNotificationsAreDisabled(true)
                 .init();
+
+        String playeriD = getAnyID();
+
     }
+
+    private String getAnyID() {
+        OSPermissionSubscriptionState status = OneSignal.getPermissionSubscriptionState();
+        //String: the OS Player Id or null if device has not registered with OS Servers
+        return status.getSubscriptionStatus().getUserId();//String: the OS Player Id or null if device has not registered with OS Servers
+
+    }
+
+
 }
